97657436f7e0c592d5582aeeade1342046873c82
XLittleLeft <XLittleLeft@Gmail.com>
2022-12-15 22:08:14 +0800

    添加项目文件。

---
+++ RoundSupplies/RoundSupplies/Config.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace RoundSupplies.RoundSupplies
+{
+    internal class Config
+    {
+        [Description("数量")]
+        public ushort ItemNumber { get; set; } = 1;
+    }
+}
---
+++ RoundSupplies/RoundSupplies/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using MEC;
+using PlayerRoles;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+
+namespace RoundSupplies.RoundSupplies
+{
+    internal class Plugin
+    {
+        [PluginEntryPoint("RoundSupplies","1.0","give something to class d when round start","X小左(XLittleLeft)")]
+        void Enabled()
+        {
+            EventManager.RegisterEvents(this);
+        }
+        [PluginEvent(ServerEventType.PlayerSpawn)]
+        void onPlayerSpawn(Player player,RoleTypeId roleTypeId)
+        {
+            Timing.CallDelayed(4f, () =>
+            {
+                if (roleTypeId.GetTeam() != Team.SCPs)
+                {
+                    switch (roleTypeId)
+                    {
+                        case RoleTypeId.ClassD:
+                            player.AddAmmo(ItemType.KeycardJanitor, Config.ItemNumber);
+                            player.AddAmmo(ItemType.Flashlight, Config.ItemNumber);
+                            break;
+                    }
+                }
+            });
+        }
+
+
+        [PluginConfig]
+        public Config Config;
+
+    }
+}
