870c444967dc7630a2142efd51098a852ec2e4fc
XLittleLeft <XLittleLeft@Gmail.com>
2022-12-15 22:37:00 +0800

    update

--- RoundSupplies/RoundSupplies/Plugin.cs
+++ RoundSupplies/RoundSupplies/Plugin.cs
@@ -1,7 +1,9 @@
-﻿using MEC;
+﻿using InventorySystem;
+using MEC;
 using PlayerRoles;
 using PluginAPI.Core;
 using PluginAPI.Core.Attributes;
+using PluginAPI.Core.Items;
 using PluginAPI.Enums;
 using PluginAPI.Events;
 
@@ -17,21 +19,20 @@ namespace RoundSupplies.RoundSupplies
         [PluginEvent(ServerEventType.PlayerSpawn)]
         void onPlayerSpawn(Player player,RoleTypeId roleTypeId)
         {
-            Timing.CallDelayed(4f, () =>
+            Timing.CallDelayed(1f, () =>
             {
                 if (roleTypeId.GetTeam() != Team.SCPs)
                 {
                     switch (roleTypeId)
                     {
                         case RoleTypeId.ClassD:
-                            player.AddAmmo(ItemType.KeycardJanitor, Config.ItemNumber);
-                            player.AddAmmo(ItemType.Flashlight, Config.ItemNumber);
+                            player.ReferenceHub.inventory.ServerAddItem(ItemType.KeycardJanitor, Config.ItemNumber);
+                            player.ReferenceHub.inventory.ServerAddItem(ItemType.Flashlight, Config.ItemNumber);
                             break;
                     }
                 }
             });
         }
-
 
         [PluginConfig]
         public Config Config;
